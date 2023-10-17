8ac43dd09edfbd60a95ba1b0bd77114e1b5655c7
TEJAS RAJENDRA THONGE <80546718+tejasthonge@users.noreply.github.com>
2023-10-17 13:59:37 +0530

    Add files via upload

---
+++ 0DailyFlash/q3.java
@@ -0,0 +1,24 @@
+/*
+
+   Que 3 : WAP to check whether the given no is odd or even
+   */
+
+
+import java.util.*;
+class x{
+
+	public static void main(String[] boss){
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.print("enter the no : ");
+
+	int n= sc.nextInt();
+
+	if(n%2 ==0)
+		System.out.println(n + " is even");
+	else
+		System.out.println(n+" is odd");
+	
+
+	}
+}
